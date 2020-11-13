8e30f444aa8b79df6054e89519868b6512303c9a
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-11-12 19:23:13 -0800

    updated files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Gallery from "./pages/Gallery"
+
+function App() {
+  return (
+    <div className="App">
+        < Gallery />
+    </div>
+  );
+}
+
+export default App;
